64a5eab1d6095c423defe1be6e9b2df8821441c2
dietflowoficial <dietflowoficial@gmail.com>
2025-12-05 22:56:48 -0300

    fix: restore missing group tag in CatTank

--- src/game/Player/CatTank.tsx
+++ src/game/Player/CatTank.tsx
@@ -497,6 +497,7 @@ export const CatTank = () => {
                         <meshBasicMaterial visible={false} />
                     </mesh>
                 </group>
-            </>
-            );
+            </group>
+        </>
+    );
 };
